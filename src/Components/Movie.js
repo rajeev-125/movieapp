b7b8e5214f8c2db528437d37a04c98d19373cadd
rajeev125 <rajeevkrsingh1202@gmail.com>
2021-12-29 19:57:24 +0530

    gitint

---
+++ src/Components/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Movielist from './Movielist'
+
+export default function Movie() {
+    return (
+        <div>
+            <Movielist
+             imgurl={}
+            title={}
+            year={}
+            boxOffice={}
+            ImdbRating={}
+            />
+            
+        </div>
+    )
+}
